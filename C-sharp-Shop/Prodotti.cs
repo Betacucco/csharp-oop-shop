3efe7700586b5144e0f0e789e2282e262d0dc193
MarcoPieretti <marcopieretti50@gmail.com>
2022-04-08 17:38:03 +0200

    Add project files.

---
+++ C-sharp-Shop/Prodotti.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_Shop
+{
+    internal class Prodotto
+    {
+        private int codice;
+        private string nome;
+        private string descrizione;
+        private double prezzo;
+        private int iva;
+
+        public Prodotto(string nome, string descrizione, double prezzo, int iva)
+        {
+            Random r = new Random();
+            int codice = r.Next(10000000, 100000000);
+            this.codice = codice;
+
+            this.nome = nome;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+            this.iva = iva;
+        }
+
+        public int GetCodice()
+        {
+            return codice;
+        }
+
+        public int GetIva()
+        {
+            return iva;
+        }
+
+        public void SetIva(int iva)
+        {
+            this.iva = iva;
+        }
+
+        public string GetNome()
+        {
+            return nome;
+        }
+
+        public void SetNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public string GetDescrizione()
+        {
+            return descrizione;
+        }
+
+        public void SetDescrizione(string descrizione)
+        {
+            this.descrizione = descrizione;
+        }
+
+        public double GetPrezzo()
+        {
+            return prezzo;
+        }
+
+        public void SetPrezzo(double prezzo)
+        {
+            this.prezzo = prezzo;
+        }
+
+        private string PrezzoIvato()
+        {
+            double ivaSulPrezzo = prezzo + (prezzo * iva / 100);
+            string strIva = ivaSulPrezzo.ToString("N2");
+            return strIva;
+        }
+
+        private string NomeEsteso()
+        {
+            string nomePiuCodice = codice + nome;
+            return nomePiuCodice;
+        }
+
+        //METODO PER STAMPARE 
+        public void StampaProdottiSenzaIva()
+        {
+            Console.WriteLine("Codice: " + codice);
+            Console.WriteLine("Nome prodotto: " + nome);
+            Console.WriteLine("Descrizione prodotto: " + descrizione);
+            Console.WriteLine("Prezzo: " + prezzo + "$");
+        }
+
+        public void StampaProdottiConIva()
+        {
+            Console.WriteLine("Codice: " + codice);
+            Console.WriteLine("Nome prodotto: " + nome);
+            Console.WriteLine("Descrizione prodotto: " + descrizione);
+            Console.WriteLine("IVA: " + iva + "%");
+            Console.WriteLine("Prezzo ivato: " + PrezzoIvato() + "$");
+        }
+
+        public void StampaProdottiConcatenati()
+        {
+            Console.WriteLine("Codice e nome del prodotto: " + NomeEsteso());
+            Console.WriteLine("Descrizione prodotto: " + descrizione);
+            Console.WriteLine("IVA: " + iva + "%");
+            Console.WriteLine("Prezzo ivato: " + PrezzoIvato() + "$");
+        }
+    }
+}
---
+++ C-sharp-Shop/Program.cs
@@ -0,0 +1,43 @@
+﻿using C_sharp_Shop;
+
+Console.WriteLine("Inserisci IVA: ");
+int nostraIva = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Inserisci nome prodotto: ");
+string nomeProdotto = Console.ReadLine();
+
+Console.WriteLine("Inserisci descrizione prodotto: ");
+string descrizioneProdotto = Console.ReadLine();
+
+Console.WriteLine("Inserisci prezzo prodotto: ");
+double prezzoProdotto = double.Parse(Console.ReadLine());
+
+
+Prodotto PrimoProdotto = new Prodotto(nomeProdotto, descrizioneProdotto, prezzoProdotto, nostraIva);
+
+Console.WriteLine("---------PRODOTTO 1--------------");
+PrimoProdotto.StampaProdottiSenzaIva();
+Console.WriteLine("-----------------------");
+PrimoProdotto.StampaProdottiConIva();
+Console.WriteLine("-----------------------");
+PrimoProdotto.StampaProdottiConcatenati();
+Console.WriteLine();
+
+Prodotto SecondoProdotto = new Prodotto("Alette Di Pollo", "Carne bianca", 6.99, 10);
+
+Console.WriteLine("---------PRODOTTO 2--------------");
+SecondoProdotto.StampaProdottiSenzaIva();
+Console.WriteLine("-----------------------");
+SecondoProdotto.StampaProdottiConIva();
+Console.WriteLine("-----------------------");
+SecondoProdotto.StampaProdottiConcatenati();
+Console.WriteLine();
+
+Prodotto TerzoProdotto = new Prodotto("Gin", "Alcolico", 26.78, 22);
+
+Console.WriteLine("---------PRODOTTO 3--------------");
+TerzoProdotto.StampaProdottiSenzaIva();
+Console.WriteLine("-----------------------");
+TerzoProdotto.StampaProdottiConIva();
+Console.WriteLine("-----------------------");
+TerzoProdotto.StampaProdottiConcatenati();
